7d569fdc6617aa3d1f7531f723ac0260fcc9f64f
Ryan Houck <ryan@levelskies.com>
2014-12-15 10:38:11 -0800

    updated check for secrety key

--- market/settings.py
+++ market/settings.py
@@ -36,7 +36,7 @@ if 'test' in sys.argv:
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = environ.get('SECRET_KEY') #os.environ['SECRET_KEY']
 
 
 # Application definition
